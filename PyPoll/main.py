7951386f49e63375dc861489ac91c77397320ced
littleprincecurry <scotwilson@gmail.com>
2020-09-16 17:34:17 -0400

    cleaned up txt writing and added comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,19 @@
+# import modules
 import os
 import csv
 
-# 
+# create a pathfile for the data file using the os module 
 csv_election = os.path.join("Resources", "election_data.csv")
 
+# create empty variables to store data in
 vote_total = 0
 candidateList = []
 voteList = []
 votes = 0
 countList = []
 
-
+# make data file readable with csv module
+# count total number of votes and create a list of candidates
 with open(csv_election, "r") as csv_file:
     election_reader = csv.reader(csv_file, delimiter=",")
     header = next(election_reader)
@@ -21,7 +24,7 @@ with open(csv_election, "r") as csv_file:
         if rows[2] not in candidateList:
             candidateList.append(rows[2])
 
-
+# tally up votes for each candidate
 for candidate in candidateList:
     for vote in voteList:
         if vote == candidate:
@@ -29,11 +32,13 @@ for candidate in candidateList:
     countList.append(votes)
     votes = 0 
 
+# create a dictionary associating candidates with vote total
 voteDict = {candidateList[i]: countList[i] for i in range(len(candidateList))} 
 
-
+# determine winner using max function on dictionary
 winner = max(voteDict, key=voteDict.get)
 
+# print election results to console
 print('Election Results')
 print('-----------------------------------')                
 print(f"Total Votes: {vote_total}")
@@ -42,17 +47,22 @@ for candidate in candidateList:
     print(f'{candidate}: {round(voteDict.get(candidate) / vote_total * 100, 3)}% ({voteDict.get(candidate)})')
 print('-----------------------------------')  
 print(f'Winner: {winner}')
-print('-----------------------------------')  
+print('-----------------------------------')
 
+# create a text file containing election results
 file_output = os.path.join('analysis', 'results.txt')
-with open(file_output, 'a') as file_writer:
-    file_writer.write('Election Results \n')
-    file_writer.write('----------------------------------- \n')                
-    file_writer.write(f"Total Votes: {vote_total} \n")
-    file_writer.write('----------------------------------- \n')  
+with open(file_output, 'w') as file_writer:
+    file_writer.write(
+        'Election Results \n'
+        '----------------------------------- \n'                
+        f"Total Votes: {vote_total} \n"
+        '----------------------------------- \n'
+        )  
     for candidate in candidateList:
         file_writer.write(f'{candidate}: {round(voteDict.get(candidate) / vote_total * 100, 3)}% ({voteDict.get(candidate)}) \n')
-    file_writer.write('----------------------------------- \n')  
-    file_writer.write(f'Winner: {winner}\n')
-    file_writer.write('-----------------------------------\n')  
+    file_writer.write(
+        '----------------------------------- \n'  
+        f'Winner: {winner}\n'
+        '-----------------------------------\n'
+        )  
     file_writer.close()
